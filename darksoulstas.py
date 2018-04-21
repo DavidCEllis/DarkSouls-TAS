116082a863f4cfb3f25e9f8e75ac4e2fa08f1968
David Ellis <ducksual@gmail.com>
2018-04-21 16:30:08 +0100

    Increment version number.

--- darksoulstas.py
+++ darksoulstas.py
@@ -12,7 +12,7 @@ from ds_tas import KeySequence, KeyPress, basics
 from ds_tas.scripts import menus, glitches, timers
 from ds_tas.engine import tas
 
-__version__ = '2.1.1'
+__version__ = '3.0.0'
 
 # Variable names to skip
 skip_vars = ['sys', 'code', 'copy', 'textwrap',
